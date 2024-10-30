4c951d4a637026cc0e6869573d56a3b015a04425
Caio Filizola <caio.cesar.filizola@accenture.com>
2024-10-30 20:41:18 +0000

    Aula 2 - Navegacao, filtros e eventos

---
+++ webapp/controller/Object.controller.js
@@ -0,0 +1,41 @@
+sap.ui.define([
+    "./BaseController",
+    "../model/formatter",
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator"
+],
+    function (BaseController, formatter, JSONModel, Filter, FilterOperator) {
+        "use strict";
+
+        return BaseController.extend("acn.btpui5treinamento.controller.Object", {
+
+            formatter: formatter,
+
+            onInit: function () {
+                debugger
+                this.setModel(new JSONModel({}), "objectModel")
+                this.getRouter().getRoute("RouterObject").attachMatched(this._onObjectMatched, this)
+            },
+
+            _onObjectMatched: function(oEvent){
+                debugger
+                var sCPF = oEvent.getParameter("arguments").CPF
+
+                this.getModel().read(`/Usuarios(${sCPF})`, {
+                    urlParameters: {
+                        "$expand": "veiculos"
+                    },
+                    success: oData => {
+                        debugger
+                        this.getModel("objectModel").setData(oData)
+                    },
+                    error: err => {
+                        debugger
+                        console.log(err)
+                    }
+                })
+
+            }
+        });
+    });
--- webapp/controller/Worklist.controller.js
+++ webapp/controller/Worklist.controller.js
@@ -1,16 +1,94 @@
 sap.ui.define([
     "./BaseController",
-    "../model/formatter"
+    "../model/formatter",
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator"
 ],
-function (BaseController, formatter) {
-    "use strict";
+    function (BaseController, formatter, JSONModel, Filter, FilterOperator) {
+        "use strict";
 
-    return BaseController.extend("acn.btpui5treinamento.controller.Worklist", {
+        return BaseController.extend("acn.btpui5treinamento.controller.Worklist", {
 
-        formatter : formatter,
+            formatter: formatter,
 
-        onInit: function () {
+            onInit: function () {
+                this.setModel(new JSONModel({
+                    nome: "",
+                    sexo: "",
+                    createdBy: ""
+                }), "oFilterModel")
 
-        }
+                this.setModel(new JSONModel({
+                    CurrentDate: new Date()
+                }), "oDateControl")
+            },
+
+            stringParaData: function (dataString) {
+                const aCuts = dataString.split("/")
+                const data = new Date(aCuts[2], aCuts[1] - 1, aCuts[0])
+
+                return data
+            },
+
+            convertToEdmDateTime: function (Date) {
+                var iso = Date.toISOString()
+                var trim = `${iso.slice(0, -5)}z`
+
+                return trim
+            },
+
+            onSearch: function (oEvent) {
+                debugger
+                var oSmart = this.getView().byId("smtUser")
+                oSmart.rebindTable(true)
+            },
+
+            onRebindTable: function (oEvent) {
+                debugger
+                //Smart Table
+                var oBindinParams = oEvent.getParameter("bindingParams")
+                //Parametros de tela
+                var oFilterModel = this.getModel("oFilterModel").getData()
+
+                if (oBindinParams) {
+                    var aFields = Object.getOwnPropertyNames(oFilterModel)
+                    if (aFields.length > 0) {
+                        aFields.forEach(campo => {
+                            if (oFilterModel[campo]) {
+                                if (campo === 'nome') {
+                                    oBindinParams.filters.push(new Filter(`${campo}`, FilterOperator.Contains, oFilterModel[campo]))
+                                } else {
+                                    if (campo === 'sexo') {
+                                        oBindinParams.filters.push(new Filter(`${campo}`, FilterOperator.EQ, parseInt(oFilterModel[campo])))
+                                    } else {
+                                        oBindinParams.filters.push(new Filter(`${campo}`, FilterOperator.EQ, oFilterModel[campo]))
+                                    }
+                                }
+                            }
+                        })
+                    }
+
+                    var oDtrCreate = this.getView().byId("dtrCreate").getValue()
+                    if (oDtrCreate) {
+                        var aDates = oDtrCreate.split('-')
+                        aDates[0] = aDates[0].replace(/[" "]/g, "")
+                        aDates[1] = aDates[1].replace(/[" "]/g, "")
+                        if (aDates[0] === aDates[1]) {
+                            oBindinParams.filters.push(new Filter("createdAt", FilterOperator.EQ, this.convertToEdmDateTime(this.stringParaData(aDates[0]))))
+                        } else {
+                            oBindinParams.filters.push(new Filter("createdAt", FilterOperator.BT, this.convertToEdmDateTime(this.stringParaData(aDates[0])), this.convertToEdmDateTime(this.stringParaData(aDates[1]))))
+                        }
+                    }
+                }
+            },
+            onCallNextScreen: function(oEvent){
+                debugger
+                var sCPF = oEvent.getSource().getBindingContext().getObject().cpf
+
+                this.getRouter().navTo("RouterObject", {
+                    CPF: sCPF
+                })
+            }
+        });
     });
-});
